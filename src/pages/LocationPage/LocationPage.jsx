cdfb52e605e1fc760c267cfccffb805033d7ae41
Sergialys Ramos <seergialysramos@gmail.com>
2024-04-01 20:56:45 +0200

    añado mapa funcional

--- src/pages/LocationPage/LocationPage.jsx
+++ src/pages/LocationPage/LocationPage.jsx
@@ -1,74 +1,72 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { Box, Flex, Grid, Text } from '@chakra-ui/react';
 import { JackInTheBox } from 'react-awesome-reveal';
-const API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
 
-class LocationPage extends Component {
-  componentDidMount() {
-    window.scrollTo(0, 0);
-    this.initMap();
-  }
+const API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 
-  initMap = () => {
-    // Eliminar cualquier script de la API de Google Maps existente
-    const existingScript = document.querySelector('[src^="https://maps.googleapis.com/maps/api/js"]');
-    if (existingScript) {
-      existingScript.remove();
-    }
-  
-    const script = document.createElement('script');
-    script.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&libraries=places`;
-    script.defer = true;
-    script.async = true;
-    script.onerror = () => {
-      console.error('Error al cargar el script de la API de Google Maps.');
-    };
-    
-    script.onload = () => {
-      // Aquí puedes utilizar la API de Google Maps, por ejemplo, crear el mapa y agregar marcadores
-      const map = new window.google.maps.Map(document.getElementById('map'), {
-        center: { lat: 40.40425297456302, lng: -3.702034253838755 },
-        zoom: 16
-      });
-  
-      new window.google.maps.Marker({
-        position: { lat: 40.40425297456302, lng: -3.702034253838755 },
-        map: map,
-        title: 'Barbershop Location'
-      });
-    };
-  
-    document.head.appendChild(script);
+const LocationPage = () => {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: API_KEY
+  });
+
+  const containerStyle = {
+    width: '500px',
+    height: '500px'
   };
-  
 
-  render() {
-    return (
-      <Box width={'90%'} backgroundColor={'rgba(67, 66, 65, 2)'} margin={'30px auto'} padding={'70px'} borderRadius={'10px'} marginTop={'150px'}>
-        <Grid templateColumns="repeat(2, 1fr)" marginBottom={'100px'}>
-          <Box textAlign={'center'} fontFamily={'sans-serif'} fontSize={'20px'} padding={'40px'}>
-            <JackInTheBox>
-              <Text fontFamily={'Martel'} color={'(255, 249, 249, 0.5)'} fontWeight={'bold'} fontSize={'20px'} marginBottom={'60px'}>VISIT OUR BARBERSHOP</Text>
-              <Text textAlign={'center'} fontFamily={'Martel'} fontSize={'36px'} fontWeight={'bold'} color={'white'} marginBottom={'60px'}>Calle de Embajadores, Centro 28012.</Text>
-              <Flex justifyContent={'center'} marginBottom={'80px'}>
-                <img src='/images/locationgold.svg' alt="location" width={'90px'} height={'90px'} />
-              </Flex>
-              <Text color={'gray'} fontFamily={'sans-serif'}>Te aseguramos que no te dejará indiferente. Nuestro mayor objetivo, la satisfacción de nuestros clientes.</Text>
-            </JackInTheBox>
-          </Box>
-          <Box width={'95%'} alignContent={'center'} margin={'0 auto'}>
-            <div id="map" style={{ height: "400px", width: "100%" }} />
-          </Box>
-        </Grid>
-      </Box>
-    );
-  }
-}
+  const center = useMemo(() => ({
+    lat: 40.40425297456302,
+    lng: -3.702034253838755
+  }), []);
+
+  const [map, setMap] = useState(null);
+
+  useEffect(() => {
+    if (isLoaded && map !== null) {
+      map.setZoom(16); // Establece el nivel de zoom
+      map.setCenter(center); // Centra el mapa en la posición especificada
+    }
+  }, [isLoaded, map, center]);
+
+  const onLoad = React.useCallback(function callback(map) {
+    setMap(map);
+  }, []);
+
+  const onUnmount = React.useCallback(function callback() {
+    setMap(null);
+  }, []);
+
+  return (
+    <Box width={'90%'} backgroundColor={'rgba(67, 66, 65, 2)'} margin={'30px auto'} padding={'70px'} borderRadius={'10px'} marginTop={'150px'}>
+      <Grid templateColumns="repeat(2, 1fr)" marginBottom={'100px'}>
+        <Box textAlign={'center'} fontFamily={'sans-serif'} fontSize={'20px'} padding={'40px'}>
+          <JackInTheBox>
+            <Text fontFamily={'Martel'} color={'(255, 249, 249, 0.5)'} fontWeight={'bold'} fontSize={'20px'} marginBottom={'60px'}>VISIT OUR BARBERSHOP</Text>
+            <Text textAlign={'center'} fontFamily={'Martel'} fontSize={'36px'} fontWeight={'bold'} color={'white'} marginBottom={'60px'}>Calle de Embajadores, Centro 28012.</Text>
+            <Flex justifyContent={'center'} marginBottom={'80px'}>
+              <img src='/images/locationgold.svg' alt="location" width={'90px'} height={'90px'} />
+            </Flex>
+            <Text color={'gray'} fontFamily={'sans-serif'}>Te aseguramos que no te dejará indiferente. Nuestro mayor objetivo, la satisfacción de nuestros clientes.</Text>
+          </JackInTheBox>
+        </Box>
+        <Box width={'95%'} alignContent={'center'} margin={'0 auto'}>
+          {isLoaded && (
+            <GoogleMap
+              center={center}
+              zoom={16}
+              mapContainerStyle={containerStyle}
+              onLoad={onLoad}
+              onUnmount={onUnmount}
+            >
+              <Marker position={center} />
+            </GoogleMap>
+          )}
+        </Box>
+      </Grid>
+    </Box>
+  );
+};
 
 export default LocationPage;
-
-
-
-
-
-
